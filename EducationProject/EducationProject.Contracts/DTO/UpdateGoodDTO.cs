1e10dddac46aa19b19f253c8fd475792e7196152
ASC\Tolmacheva <tolmacheva-dashulya@list.ru>
2026-07-20 16:57:35 +0500

    начала создавать проект с контрактными моделями

---
+++ EducationProject/EducationProject.Contracts/DTO/UpdateGoodDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace EducationProject.API.DTO
+{
+    public class UpdateGoodDTO
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal FinalPrice { get; set; }
+        public string? Description { get; set; }
+        public int Count { get; set; }
+        public int Discount { get; set; }
+    }
+}
